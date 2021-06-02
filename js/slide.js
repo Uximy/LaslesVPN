3d076392e6a3516c343f50ee5803cbe12913fd10
Uximy <Uximy6553@gmail.com>
2021-06-02 16:32:24 +0600

    [Code Rework]

--- js/slide.js
+++ js/slide.js
@@ -1,21 +1,19 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    nav:true,
-    autoplay: true,
-    navText: ['<i class="fas fa-arrow-left btn_slider"></i>','<i class="fas fa-arrow-right btn_slider"></i>'],
-    dotsEach: true,
-    mouseDrag: false,
-    autoplayTimeout: 6500,
-    smartSpeed: 350,
-    center: true,
-    autoWidth: true,
-    responsive:{
-        0:{
-            items:1,
-            dotsData: true,
-        },
-        600:{
-            items:3,
+$(document).ready(function(){
+        $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 15,
+        nav:false,
+        navText: ['<i class="fas fa-arrow-left btn_slider"></i>','<i class="fas fa-arrow-right btn_slider"></i>'],
+        dotsEach: true,
+        mouseDrag: false,
+        autoplay: true,
+        autoplaySpeed:1500,
+        autoplayTimeout: 6500,
+        autoWidth: true,
+        responsive:{
+            600:{
+                items:3,
+            }
         }
-    }
-})
+    })
+});
